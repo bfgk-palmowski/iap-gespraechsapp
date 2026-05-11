1b2cf72e04ee2d75fe8b07d9a63caa36cdbd2f86
bfgk-palmowski <organspendepaten@gmail.com>
2026-05-11 17:25:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -516,7 +516,7 @@ export default function App() {
           {/* IAP Logo */}
           <div style={{ marginBottom: '24px' }}>
             <img 
-              src="/testapp/img/iap-logo.png"
+              src="./img/iap-logo.png"
               alt="IAP – Lehrstuhl für die Ausbildung personaler und interpersonaler Kompetenzen im Gesundheitswesen, Universität Witten/Herdecke"
               style={{ 
                 width: '100%', maxWidth: '220px',
